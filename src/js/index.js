8a0d8a566eca933b56f6b1a5aa698ac84657ede8
Science-B <dmitriytwd@gmail.com>
2023-08-28 13:52:10 +0300

    added popular_tours section

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
 import mobileNav from "./modules/mobile-nav.js";
 import getAirDatePicker from "./modules/air-datepicker.js";
+import getPopularToursSwiper from "./modules/swiper.js";
 
+getPopularToursSwiper();
 getAirDatePicker();
 mobileNav();
