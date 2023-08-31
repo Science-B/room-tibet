ba5111a99db43c63770c0e90b0141b2324254ab1
Science-B <dmitriytwd@gmail.com>
2023-08-31 12:59:07 +0300

    added blog-section

--- src/js/modules/swiper.js
+++ src/js/modules/swiper.js
@@ -5,11 +5,7 @@ function getPopularToursSwiper() {
 	const swiper = new Swiper(".popular__tours-swiper", {
 		slidesPerView: "auto",
 		grabCursor: true,
-		slidesPerView: 3,
 		spaceBetween: 20,
-		autoplay: {
-			delay: 5000,
-		},
 	});
 }
 
