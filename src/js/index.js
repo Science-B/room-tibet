e2bae9f81db994bd299b6590f0f438b2fd846af5
Science-B <dmitriytwd@gmail.com>
2023-09-02 14:54:46 +0300

    added fix-header-bugs

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,9 @@
 import mobileNav from "./modules/mobile-nav.js";
 import getAirDatePicker from "./modules/air-datepicker.js";
 import getPopularToursSwiper from "./modules/swiper.js";
+import Choices from "choices.js";
 
+customSelect("select");
 getPopularToursSwiper();
 getAirDatePicker();
 mobileNav();
